09eb3e8bc0943264c464c9910a223579a47eddec
priyanka <priyankachettu94@gmail.com>
2021-04-23 08:38:36 +0530

    correction made

--- js/marks.js
+++ js/marks.js
@@ -1,6 +1,7 @@
+
 var x=document.getElementsByClassName("yes")
-var y=document.getElementsByClassNames("no")
-var z =ocument.getElementsByClassNames("dont know")
+var y=document.getElementsByClassName("no")
+var z =ocument.getElementsByClassName("dont know")
      function valueretrieval(){
                
                 var i
@@ -28,4 +29,18 @@ var z =ocument.getElementsByClassNames("dont know")
               console.log(percentcountd)
             
 
-            }
+            
+            var f=document.getElementById("noofyes")
+            var yesno= document.createElement("div")
+            yesno.appendChild(document.createTextNode(percentcounty))
+            f.appendChild(yesno)
+            var p=document.getElementById("noofno")
+            var nono=document.createElement("div")
+            nono.appendChild(document.createTextNode(percentcountn))
+            p.appendChild(nono)
+            
+            var v=document.getElementById("noofdontknow")
+            var dontknowno =document.createElement("div")
+            dontknowno.appendChild(document.createTextNode(percentcountd))
+            v.appendChild(dontknowno)
+     }
